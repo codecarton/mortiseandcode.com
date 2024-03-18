576be5750652525ef92867c8e9896181541c0684
Jordan Calhoun <jordancalhoun@gmail.com>
2024-03-18 11:01:02 -0400

    Remove extra import statement

--- src/routes/blog/category/[category]/+page.svelte
+++ src/routes/blog/category/[category]/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-  import type { PageData } from './$types';
-  import VerticalBlogCard from '$lib/components/blog/VerticalBlogCard.svelte';
-  import MortiseCode from '$lib/components/blog/MortiseCode.svelte';
-  export let data: PageData;
+	import type { PageData } from './$types';
+	import VerticalBlogCard from '$lib/components/blog/VerticalBlogCard.svelte';
+	export let data: PageData;
 </script>
 
 <MortiseCode />
 
 <div class="text-center text-sm text-neutral-400 my-5">
-  Filtering by:
-  <a
-    href="/blog"
-    alt="Blog Index"
-    class="
+	Filtering by:
+	<a
+		href="/blog"
+		alt="Blog Index"
+		class="
     px-2
     py-1
     mx-2
@@ -25,13 +24,13 @@
     hover:bg-neutral-300
     hover:text-neutral-700
     uppercase"
-  >
-    {data.category} &#x2715
-  </a>
+	>
+		{data.category} &#x2715
+	</a>
 </div>
 
 <div
-  class="
+	class="
     mx-auto
     md:max-w-5xl
     lg:max-w-7xl
@@ -41,7 +40,7 @@
     p-5
     justify-items-center"
 >
-  {#each data.posts as post}
-    <VerticalBlogCard {post} />
-  {/each}
+	{#each data.posts as post}
+		<VerticalBlogCard {post} />
+	{/each}
 </div>
